db31b91c04e4138f70dbd5ede94c1b13d543d2aa
DanielKnaub <103882875+DanielKnaub@users.noreply.github.com>
2023-05-27 09:32:16 +0000

    Added env variables

--- melontime/settings.py
+++ melontime/settings.py
@@ -12,7 +12,9 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from django.utils.translation import gettext_lazy as _
 from pathlib import Path
-from .tokenkey import TOKEN
+
+import environ
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,8 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
+# Initialize environment variables
+env = environ.Env()
+environ.Env.read_env()
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = TOKEN
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
